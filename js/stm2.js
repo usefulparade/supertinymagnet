b03c75b061a98dcbda8c528b4dc49c2df08b25fe
Luke <luke@lukewilliamsmusic.com>
2019-05-14 16:35:49 -0400

    frames, much better layout, feeling happy

--- js/stm2.js
+++ js/stm2.js
@@ -1,31 +1,31 @@
-var bgvid;
-var playhead;
-var vidLoaded;
+// var bgvid;
+// var playhead;
+// var vidLoaded;
 
-function preload(){
-    bgvid = createVideo("./assets/SAMloop-luke-edit-2.mp4", loadVid);
-    bgvid.parent('bgvid');
-}
+// function preload(){
+//     bgvid = createVideo("./assets/SAMloop-luke-edit-2.mp4", loadVid);
+//     bgvid.parent('bgvid');
+// }
 
-function setup(){
-    playhead = 0.1;
-    vidLoaded = false;
-}
+// function setup(){
+//     playhead = 0.1;
+//     vidLoaded = false;
+// }
 
-function draw(){
-}
+// function draw(){
+// }
 
-function windowResized(){
-}
+// function windowResized(){
+// }
 
-function loadVid(){
-    bgvid.volume(0);
-    bgvid.attribute('width', '');
-    bgvid.attribute('height', '');
-    //bgvid.pause();
-    vidLoaded = true;
-    bgvid.loop();
-}
+// function loadVid(){
+//     bgvid.volume(0);
+//     bgvid.attribute('width', '');
+//     bgvid.attribute('height', '');
+//     //bgvid.pause();
+//     vidLoaded = true;
+//     bgvid.loop();
+// }
 
 function scrollOnClick(_x,_y){
     window.scrollTo(_x,_y);
