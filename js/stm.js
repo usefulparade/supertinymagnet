193d66523d89db5199b82b1d8799d4dc108196e5
usefulparade <singingland@gmail.com>
2019-06-20 19:37:26 -0400

    copy edits, spinning plus sign, new loop

--- js/stm.js
+++ js/stm.js
@@ -1,31 +1,4 @@
-// var bgvid;
-// var playhead;
-// var vidLoaded;
-
-// function preload(){
-//     bgvid = createVideo("./assets/SAMloop-luke-edit-2.mp4", loadVid);
-//     bgvid.parent('bgvid');
-// }
-
-// function setup(){
-//     playhead = 0.1;
-//     vidLoaded = false;
-// }
-
-// function draw(){
-// }
-
-// function windowResized(){
-// }
-
-// function loadVid(){
-//     bgvid.volume(0);
-//     bgvid.attribute('width', '');
-//     bgvid.attribute('height', '');
-//     //bgvid.pause();
-//     vidLoaded = true;
-//     bgvid.loop();
-// }
+var turned = false;
 
 function scrollOnClick(_x,_y){
     window.scrollTo(_x,_y);
@@ -33,4 +6,14 @@ function scrollOnClick(_x,_y){
 
 function toggleNav(){
     document.getElementById("subnav").classList.toggle("subnav-open");
+}
+
+function plusToX(){
+    if (!turned) {
+        document.getElementById("toggle-plus").style.animation = "plus-to-x 0.2s 1 forwards";
+        turned = true;
+    } else {
+        document.getElementById("toggle-plus").style.animation = "x-to-plus 0.2s 1 forwards";
+        turned = false;
+    }
 }
